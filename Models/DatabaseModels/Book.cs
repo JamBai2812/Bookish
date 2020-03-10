81ef02146aeab435e096f3c57676c8998515d241
Jamie Baird <james.baird@softwire.com>
2020-03-10 14:15:06 +0000

    Moved models and created BookModel

--- Models/DatabaseModels/Book.cs
+++ Models/DatabaseModels/Book.cs
@@ -5,7 +5,8 @@ namespace Bookish.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Author { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
         public string Title { get; set; }
         public int YearPublished { get; set; } 
         public int NumberCheckedOut { get; set; }
---
+++ Models/Models/BookModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Bookish.Models.Models
+{
+    public class BookModel
+    {
+        private Book _book;
+
+        BookModel(Book book)
+        {
+            _book.Id = book.Id;
+            _book.AuthorFirstName = book.AuthorFirstName;
+            _book.AuthorLastName = book.AuthorLastName;
+            _book.Title = book.Title;
+            _book.YearPublished = book.YearPublished;
+            _book.NumberCheckedOut = book.NumberCheckedOut;
+            _book.NumberInStock = book.NumberInStock;
+        }
+    }
+}
