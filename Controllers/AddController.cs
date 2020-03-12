c9a438ec7d7d20fd73294214c3d871db8f4bcb3d
Jamie Baird <james.baird@softwire.com>
2020-03-12 14:35:19 +0000

    Added new controller for adding books, and form to handle adding a book

---
+++ Controllers/AddController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookish.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bookish.Models;
+using Bookish.Models.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookish.Controllers
+{
+    public class AddController : Controller
+    {
+        private readonly ILogger<SearchController> _logger;
+        
+        // private readonly IService _myService;
+        //
+        // public SearchController(IService myService)
+        // {
+        //     _myService = myService;
+        // }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+        
+        
+    }
+}
