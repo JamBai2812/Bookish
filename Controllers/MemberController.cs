eb6edd0ff67a346dec7a1857276a237a49bb004d
Jamie Baird <james.baird@softwire.com>
2020-03-13 11:04:11 +0000

    Edited NavBar - members ready to be given some functionality

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -12,35 +12,30 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookish.Controllers
 {
-    
-        public class MemberController : Controller
+    public class MemberController : Controller
+    {
+        //Services
+        private readonly ILogger<BooksController> _logger;
+        
+
+        //Constructor
+        public MemberController(ILogger<BooksController> logger)
         {
-            //Services
-            private readonly ILogger<BooksController> _logger;
-            private readonly IFetcher _myService;
-            private readonly ICopyUpdater _copyUpdaterService;
-            private readonly IAdder _adderService;
-         
-            //Constructor
-            public MemberController(IFetcher myService, ICopyUpdater copyUpdaterService, IAdder adderService, ILogger<BooksController> logger)
-            {
-                _myService = myService;
-                _copyUpdaterService = copyUpdaterService;
-                _adderService = adderService;
-                _logger = logger;
-            }
-                 
-            //Actions
-            public IActionResult Search()
-            {
-                return View("Index");
-            }
-            
-            
-            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-            public IActionResult Error()
-            {
-                return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-            }
-    
+           
+            _logger = logger;
+        }
+
+        //Actions
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
 }
