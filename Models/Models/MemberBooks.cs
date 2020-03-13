30364e46b0dce9a4198c65c449d7cc45350ea559
Jamie Baird <james.baird@softwire.com>
2020-03-13 12:02:31 +0000

    Created MemberBook and MemberBooks to hold all information who has books

---
+++ Models/Models/MemberBooks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Bookish.Models.Models
+{
+    public class MemberBooks
+    {
+        public List<MemberBook> MemberBookList { get; set; }
+        
+        public MemberBooks(List<MemberBook> memberBookList)
+        {
+            MemberBookList = memberBookList;
+        }
+    }
+}
